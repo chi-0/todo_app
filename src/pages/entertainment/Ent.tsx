c7f901263bf16ccad60da1396243f185fd032b9f
chi-0 <sc0o0@naver.com>
2024-03-08 00:20:14 +0900

    ent page update

--- src/pages/entertainment/Ent.tsx
+++ src/pages/entertainment/Ent.tsx
@@ -1,5 +1,43 @@
+import styled from "styled-components";
 import { Container } from "../../ui/Container";
+import { Link } from "react-router-dom";
+import { Wavve } from "./img/Wavve";
+import { Tving } from "./img/Tving";
+import { Netflix } from "./img/Netflix";
+import { Disney } from "./img/Disney";
+import { Watcha } from "./img/Watcha";
+
+const Con = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+  padding: 30px 10px;
+`;
 
 export const Ent = () => {
-  return <Container>추후 업데이트 예정입니다(ent)</Container>;
+  return (
+    <Container>
+      <Con>
+        <Link to="https://www.wavve.com/" target="_blank">
+          <Wavve />
+        </Link>
+        <Link to="https://www.tving.com/" target="_blank">
+          <Tving />
+        </Link>
+        <Link to="https://www.netflix.com/" target="_blank">
+          <Netflix />
+        </Link>
+        <Link
+          to="https://www.disneyplus.com/"
+          target="_blank"
+          style={{ transform: "translateY(-25px)" }}
+        >
+          <Disney />
+        </Link>
+        <Link to="https://watcha.com/" target="_blank">
+          <Watcha />
+        </Link>
+      </Con>
+    </Container>
+  );
 };
